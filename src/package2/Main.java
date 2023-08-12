aaefe7f8d225c63943480435d54573c1d656f9c3
Sebastian <sealor92@gmail.com>
2023-08-12 11:10:30 -0500

    Creación de main

---
+++ src/package2/Main.java
@@ -0,0 +1,9 @@
+package src.package2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Prueba prueba = new Prueba();
+        System.out.println("Arranqué!");
+    }
+}
---
+++ src/package2/Prueba.java
@@ -0,0 +1,12 @@
+package src.package2;
+
+public class Prueba {
+
+    private Prueba(String cualquiera) {
+
+    }
+
+    void test() {
+        System.out.printf("Cualquier string");
+    }
+}
